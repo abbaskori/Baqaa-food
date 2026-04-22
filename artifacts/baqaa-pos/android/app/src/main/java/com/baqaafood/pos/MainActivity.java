7daa9bd621d77806349ae50855dd5183ac61ed32
abbaskori <chaudharyaish380@gmail.com>
2026-04-22 22:53:17 +0530

    CI: Add GitHub Action to build Android APK automatically

---
+++ artifacts/baqaa-pos/android/app/src/main/java/com/baqaafood/pos/MainActivity.java
@@ -0,0 +1,5 @@
+package com.baqaafood.pos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ artifacts/baqaa-pos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.baqaafood.pos',
+  appName: 'BaqaaPOS',
+  webDir: 'dist'
+};
+
+export default config;
